81ffb7d4d639a4518e7599ced336446081fbdf4c
aitopensource <ossaitofficial@gmail.com>
2024-08-08 10:13:32 +0530

    Made leaveForm and OutPass form

--- src/Forms/OutpassForm.jsx
+++ src/Forms/OutpassForm.jsx
@@ -1,91 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card as ChakraCard, CardHeader, CardBody, CardFooter, Heading, FormControl, FormLabel, Input, Button, Stack, InputGroup, InputLeftAddon } from '@chakra-ui/react';
-import  DatePicker  from '@fluentui/react-datepicker-compat';
-import { Field, makeStyles } from '@fluentui/react-components';
 import Navbar from '../components/navbar/navbar';
-
-// Styles for the DatePicker
-const useStyles = makeStyles({
-  control: {
-    maxWidth: '300px',
-  },
-});
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styles from './Form.module.css'; // Import the CSS module
 
 const OutpassForm = () => {
-  const styles = useStyles();
+  const [startDate, setStartDate] = useState(new Date());
+  const [startHour, setStartHour] = useState('');
+  const [endHour, setEndHour] = useState('');
 
   return (
     <>
       <Navbar />
-      <ChakraCard maxW='lg' borderWidth='1px' borderRadius='md' p={4} m={4} boxShadow='md'>
-        <CardHeader>
-          <Heading size='lg'>Outpass Request Form</Heading>
-        </CardHeader>
-        <CardBody>
-          <form>
-            <Stack spacing={4}>
-              <FormControl isRequired>
-                <FormLabel>First Name</FormLabel>
-                <Input placeholder='First name' />
-              </FormControl>
+      <div className={styles.main}>
+        <ChakraCard borderWidth='1px' borderRadius='md' p={4} boxShadow='md' w='full'>
+          <CardHeader>
+            <Heading size='lg'>Outpass Request Form</Heading>
+          </CardHeader>
+          <CardBody>
+            <form>
+              <Stack spacing={4}>
+                <FormControl isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input placeholder='First name' className={styles['chakra-input']} />
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Last Name</FormLabel>
-                <Input placeholder='Last name' />
-              </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Last Name</FormLabel>
+                  <Input placeholder='Last name' className={styles['chakra-input']} />
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Registration Number</FormLabel>
-                <Input placeholder='Registration Number' />
-              </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Registration Number</FormLabel>
+                  <Input placeholder='Registration Number' className={styles['chakra-input']} />
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Reason for Outpass</FormLabel>
-                <Input placeholder='Reason for Outpass' />
-              </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Reason for Outpass</FormLabel>
+                  <Input placeholder='Reason for Outpass' className={styles['chakra-input']} />
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Date</FormLabel>
-                <Field label="Start date">
+                <FormControl isRequired>
+                  <FormLabel>Date</FormLabel>
                   <DatePicker
-                    showWeekNumbers={true}
-                    firstWeekOfYear={1}
-                    showMonthPickerAsOverlay={true}
-                    placeholder="Select a date..."
-                    className={styles.control}
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    dateFormat='MMMM d, yyyy'
+                    className={styles['chakra-input']}
+                    wrapperClassName={styles['chakra-datepicker']}
+                    popperClassName={styles['chakra-datepicker-popover']}
                   />
-                </Field>
-              </FormControl>
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Time</FormLabel>
-                <Input type='time' />
-              </FormControl>
+                <FormControl isRequired>
+                  <FormLabel>Outing Hours</FormLabel>
+                  <Stack spacing={3}>
+                    <FormControl>
+                      <FormLabel>Start Hour</FormLabel>
+                      <Input
+                        type='time'
+                        value={startHour}
+                        onChange={(e) => setStartHour(e.target.value)}
+                        className={styles['chakra-input']}
+                      />
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel>End Hour</FormLabel>
+                      <Input
+                        type='time'
+                        value={endHour}
+                        onChange={(e) => setEndHour(e.target.value)}
+                        className={styles['chakra-input']}
+                      />
+                    </FormControl>
+                  </Stack>
+                </FormControl>
 
-              <FormControl isRequired>
-                <FormLabel>Contact Number</FormLabel>
-                <InputGroup>
-                  <InputLeftAddon children='+91' />
-                  <Input placeholder='Contact Number' />
-                </InputGroup>
-              </FormControl>
-
-              <FormControl isRequired>
-                <FormLabel>Father's Contact Number</FormLabel>
-                <InputGroup>
-                  <InputLeftAddon children='+91' />
-                  <Input placeholder="Father's Contact Number" />
-                </InputGroup>
-              </FormControl>
-            </Stack>
-          </form>
-        </CardBody>
-        <CardFooter>
-          <Button colorScheme='teal' type='submit'>
-            Submit
-          </Button>
-        </CardFooter>
-      </ChakraCard>
+                <FormControl isRequired>
+                  <FormLabel>Contact Number</FormLabel>
+                  <InputGroup>
+                    <InputLeftAddon children='+91' />
+                    <Input placeholder='Contact Number' className={styles['chakra-input']} />
+                  </InputGroup>
+                </FormControl>
+                
+                {/* Uncomment and use if needed
+                <FormControl isRequired>
+                  <FormLabel>Father's Contact Number</FormLabel>
+                  <InputGroup>
+                    <InputLeftAddon children='+91' />
+                    <Input placeholder="Father's Contact Number" className={styles['chakra-input']} />
+                  </InputGroup>
+                </FormControl> */}
+              </Stack>
+            </form>
+          </CardBody>
+          <CardFooter>
+            <Button colorScheme='teal' type='submit'>
+              Submit
+            </Button>
+          </CardFooter>
+        </ChakraCard>
+      </div>
     </>
   );
 };
